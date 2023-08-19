e966e91e29bf144a2c8887bdcaf012057226968b
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-08-19 15:22:47 +0530

    Added Controllers

---
+++ backend/Controllers/BookingController.cs
@@ -0,0 +1,52 @@
+﻿using intergalactica.Models;
+using intergalactica.Services;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace intergalactica.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookingController : ControllerBase
+{
+    private readonly BookingService _bookingService;
+
+    public BookingController(BookingService bookingService)
+    {
+        _bookingService = bookingService;
+    }
+
+    [HttpGet("user/{userId}")]
+    public ActionResult<IEnumerable<Booking>> GetUserBookings(string userId)
+    {
+        return Ok(_bookingService.GetByUser(userId));
+    }
+
+    [HttpPost("add")]
+    public ActionResult AddBooking(Booking booking)
+    {
+        _bookingService.Create(booking);
+        return NoContent();
+    }
+
+    [HttpPatch]
+    public ActionResult PatchBooking(string id, [FromBody] JsonPatchDocument<Booking> patchDoc)
+    {
+        if (patchDoc == null)
+        {
+            return BadRequest();
+        }
+
+        var booking = _bookingService.Get(id);
+        if (booking == null)
+        {
+            return NotFound();
+        }
+
+        patchDoc.ApplyTo(booking);
+
+        _bookingService.Update(id, booking);
+        return NoContent();
+    }
+
+}
---
+++ backend/Controllers/ShuttleControlller.cs
@@ -0,0 +1,36 @@
+﻿using intergalactica.Models;
+using intergalactica.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace intergalactica.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ShuttleControlller : ControllerBase
+{
+    private readonly ShuttleService _shuttleService;
+
+    public ShuttleControlller(ShuttleService shuttleService)
+    {
+        _shuttleService = shuttleService;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Shuttle>> Get()
+    {
+        return Ok(_shuttleService.Get());
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Shuttle> Get(string id)
+    {
+        var shuttle = _shuttleService.Get(id);
+
+        if (shuttle == null)
+        {
+            return NotFound();
+        }
+
+        return shuttle;
+    }
+}
