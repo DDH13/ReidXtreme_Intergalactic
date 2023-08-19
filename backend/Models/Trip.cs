8db6d666e24f980d0e0f72c0ae23f5a27054b794
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-08-19 10:48:22 +0530

    Added Models

---
+++ backend/Models/Trip.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace intergalactica.Models;
+
+public class Trip
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    [BsonElement("source")]
+    public ObjectId Source { get; set; } = new ObjectId(string.Empty);
+    [BsonElement("destination")]
+    public ObjectId Destination { get; set; } = new ObjectId(string.Empty);
+    [BsonElement("source_dock")]
+    public String SourceDock { get; set; } = string.Empty;
+    [BsonElement("destination_dock")]
+    public String DestinationDock { get; set; } = string.Empty;
+    [BsonElement("shuttle")]
+    public ObjectId Shuttle { get; set; } = new ObjectId(string.Empty);
+    [BsonElement("ticket_price")]
+    public float TicketPrice { get; set; } = 0;
+
+    //year is irrelevant, storing as a DateTime for ease of use
+    //recurring trips will have the same date and time every year
+
+    [BsonElement("departure")]
+    public DateTime Departure { get; set; } = DateTime.MinValue;
+    [BsonElement("arrival")]
+    public DateTime Arrival { get; set; } = DateTime.MinValue;
+
+
+}
---
+++ backend/Program.cs
@@ -0,0 +1,74 @@
+using System.Text;
+using intergalactica.Models;
+using intergalactica.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
+using Swashbuckle.AspNetCore.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+builder.Services.AddAuthentication(
+    options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            builder.Configuration.GetSection("AppSettings:Token").Value!)),
+    };
+});
+
+builder.Services.Configure<AtlasDbSettings>(
+    builder.Configuration.GetSection(nameof(AtlasDbSettings)));
+builder.Services.AddSingleton<IAtlasDbSettings>(sp =>
+    sp.GetRequiredService<IOptions<AtlasDbSettings>>().Value);
+builder.Services.AddSingleton<IMongoClient>(_ =>
+    new MongoClient(builder.Configuration.GetValue<string>("AtlasDbSettings:ConnectionString")));
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
