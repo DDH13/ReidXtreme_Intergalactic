1e53f9f3a2a4004ca52c36182e66a622b42fc899
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-08-19 20:47:37 +0530

    Added some services

---
+++ frontend2/lib/services/user_service.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import '../utils/routes.dart';
+import 'package:http/http.dart' as http;
+import 'local_service.dart';
+
+class UserService {
+
+  Future<bool> login(username, password) async {
+    Map<String, dynamic> data = {
+      'username': username,
+      'password': password,
+    };
+
+    String body = json.encode(data);
+
+    try {
+      // send a post request to the API endpoint
+      var response = await http.post(
+        Uri.parse(getRoute("User/login")),
+        headers: {'Content-Type': 'application/json'},
+        body: body,
+      );
+
+      if (response.statusCode == 200) {
+        LocalService.setJWT(json.decode(response.body));
+        LocalService.setCurrentUserName(username);
+        LocalService.setCurrentUserId("64aa7bcddd01ede8be01ca6c");
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+
+}
