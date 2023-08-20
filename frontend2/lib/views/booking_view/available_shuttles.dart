3520a20e8fada6162eb2a6a4415ab0fedd7f5069
venudi <venudidisara@gmail.com>
2023-08-20 21:11:23 +0530

    available shuttles front_end

--- frontend2/lib/views/booking_view/available_shuttles.dart
+++ frontend2/lib/views/booking_view/available_shuttles.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:frontend2/views/booking_view/payment.dart';
-import '../../utils/background_image.dart'; // Import the BackgroundImage component
+import 'package:frontend2/utils/detailed_card_styles.dart';
+import 'package:frontend2/views/booking_view/find_shuttle_view.dart';
+import 'package:frontend2/views/booking_view/billing.dart';
+import '../../utils/background_image.dart';
 import '../../utils/appbar_styles.dart';
+import '../../utils/elevated_button_styles.dart';
 
 class AvailableShuttles extends StatefulWidget {
-  AvailableShuttles({Key? key}) : super(key: key);
+  const AvailableShuttles({Key? key}) : super(key: key);
 
   @override
   _AvailableShuttlesState createState() => _AvailableShuttlesState();
 }
 
-class _AvailableShuttlesState extends State<AvailableShuttles> with SingleTickerProviderStateMixin {
+class _AvailableShuttlesState extends State<AvailableShuttles>
+    with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
@@ -19,38 +23,152 @@ class _AvailableShuttlesState extends State<AvailableShuttles> with SingleTicker
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title: 'Available Shuttles'),
+      appBar: const CustomAppBar(title: 'Available Shuttles'),
       body: Stack(
-        children: [
-          BackgroundImage(),
-          Center(
+          children: [
+          const BackgroundImage(),
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                SizedBox(height: 20),
-                Align(
+                const SizedBox(height: 20),
+                const Align(
                   alignment: Alignment.centerRight,
-                  //content, cards
                 ),
-                SizedBox(height: 120),
-
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
+                GestureDetector(
+                  onTap: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => Payment(),
+                        builder: (context) => Billing(),
                       ),
                     );
                   },
-                  child: const Text('< EDIT DETAILS'),
+                  child: DetailedCardTheme.createAvailableShuttleCard(
+                      'SPACEX MK1',
+                      'FIRST CLASS',
+                      '12:30',
+                      '10/12',
+                      '00:30',
+                      '12/12',
+                      '12:30',
+                      '00:30',
+                      '12/12',
+                      '12:30',
+                      '2 DAYS 12 HOURS',
+                      'IS1',
+                      'MD1',
+                      '1',
+                      '2',
+                      '50,000'
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Billing(),
+                      ),
+                    );
+                  },
+                  child: DetailedCardTheme.createAvailableShuttleCard(
+                      'SPACEX MK1',
+                      'FIRST CLASS',
+                      '12:30',
+                      '10/12',
+                      '00:30',
+                      '12/12',
+                      '12:30',
+                      '00:30',
+                      '12/12',
+                      '12:30',
+                      '2 DAYS 12 HOURS',
+                      'IS1',
+                      'MD1',
+                      '1',
+                      '2',
+                      '50,000'
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Billing(),
+                      ),
+                    );
+                  },
+                  child: DetailedCardTheme.createAvailableShuttleCard(
+                      'SPACEX MK1',
+                      'FIRST CLASS',
+                      '12:30',
+                      '10/12',
+                      '00:30',
+                      '12/12',
+                      '12:30',
+                      '00:30',
+                      '12/12',
+                      '12:30',
+                      '2 DAYS 12 HOURS',
+                      'IS1',
+                      'MD1',
+                      '1',
+                      '2',
+                      '50,000'
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Billing(),
+                      ),
+                    );
+                  },
+                  child: DetailedCardTheme.createAvailableShuttleCard(
+                      'SPACEX MK1',
+                      'FIRST CLASS',
+                      '12:30',
+                      '10/12',
+                      '00:30',
+                      '12/12',
+                      '12:30',
+                      '00:30',
+                      '12/12',
+                      '12:30',
+                      '2 DAYS 12 HOURS',
+                      'IS1',
+                      'MD1',
+                      '1',
+                      '2',
+                      '50,000'
+                  ),
                 ),
               ],
             ),
           ),
+
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: ElevatedButton(
+            style: CustomElevatedButtonTheme.customButtonTheme.style,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const FindShuttleView(),
+                  ),
+                );
+              },
+              child: const Text('< EDIT DETAILS'),
+            ),
+        ),
         ],
-      ),
-    );
+        ),
+        );
   }
 }
